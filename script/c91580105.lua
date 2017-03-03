be12b8aa9754bd095e0e95bb5912a431c3e7e75c
Matt Dennis <msd7734@rit.edu>
2017-03-02 22:56:16 -0500

    Reptilianne Ward

---
+++ script/c91580105.lua
@@ -0,0 +1,83 @@
+-- Reptilianne Ward
+function c91580105.initial_effect(c)
+	-- Activate to negate, destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(c91580105.condition)
+	e1:SetCost(c91580105.cost)
+	e1:SetTarget(c91580105.target)
+	e1:SetOperation(c91580105.activate)
+	c:RegisterEffect(e1)
+	-- Reduce ATK on destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCondition(c91580105.descon)
+	e2:SetTarget(c91580105.redtarget)
+	e2:SetOperation(c91580105.redop)
+	c:RegisterEffect(e2)
+end
+
+function c91580105.filter(c)
+	return c:IsSetCard(0x3C) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+end
+
+function c91580105.condition(e,tp,eg,ep,ev,re,r,rp)
+	return rp~=tp and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+end
+function c91580105.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(c91580105.filter,tp,LOCATION_GRAVE,0,1,nil)
+	end
+	local g=Duel.SelectMatchingCard(tp,c91580104.rfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c91580105.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function c91580105.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+
+function c91580105.redfilter(c,tp)
+	return c:GetControler()~=tp and c:IsFaceup() and c:IsType(TYPE_MONSTER)
+end
+
+function c91580105.descon(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(r,REASON_DESTROY)~=0 and rp~=tp
+		and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+		and e:GetHandler():IsPreviousPosition(POS_FACEDOWN)
+end
+
+function c91580105.redtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c91580105.redfilter,tp,0,LOCATION_MZONE,1,nil,tp) end
+	local g=Duel.SelectMatchingCard(tp,c91580104.rfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,1,0,0)
+end
+
+function c91580105.redop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc then
+		
+		return
+	end
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+	end
+end
